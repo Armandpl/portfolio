3c184ae146bb3cce8b140eda33f6cb1dd6a6e95e
Armand <armand@n0s.eu>
2019-08-20 14:26:34 +0200

    utilisation de la propriété image du frontmatter dans le layout project

--- .vuepress/theme/layouts/Project.vue
+++ .vuepress/theme/layouts/Project.vue
@@ -10,7 +10,7 @@
       <div>
         <img
           class="float-right ml-4 my-2 h-56"
-          src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+          :src="$page.frontmatter.image"
         />
         <Content />
       </div>
